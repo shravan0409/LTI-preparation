41e06027a4b5edf9a30b6910f1c42bb257304424
Shravan Srinivasan <47490781+shravan0409@users.noreply.github.com>
2021-03-16 19:58:08 +0530

    Create What's  your name.py

---
+++ Hackerrank/Strings/What's  your name.py
@@ -0,0 +1,7 @@
+def print_full_name(a, b):
+    print("Hello "+a+" "+b+"! You just delved into python.")    #print can just simply add any form of data for the printing process
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
