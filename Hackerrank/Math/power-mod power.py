962576348f8f477f81ecbf31eaaec013c58e7e32
Shravan Srinivasan <47490781+shravan0409@users.noreply.github.com>
2021-03-18 08:44:50 +0530

    Create power-mod power.py

---
+++ Hackerrank/Math/power-mod power.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = int(input())
+b = int(input())
+m = int(input())
+
+print(pow(a,b))         #returns the output as a**b
+print(pow(a,b,m))       #returns the output as (a**b)%m
