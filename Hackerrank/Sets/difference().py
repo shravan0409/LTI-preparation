47a633b28cad4d5a7f3bd51b0eb78456e0cf7435
Shravan Srinivasan <47490781+shravan0409@users.noreply.github.com>
2021-03-18 22:16:28 +0530

    Create difference().py

---
+++ Hackerrank/Sets/difference().py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n1=input()
+s1=set(input().split(" "))
+n2=input()
+s2=set(input().split(" "))
+s3=s1.difference(s2)
+print (len(s3))
