d6081d64049a0827a39ba0f8d870d4f779fb3dd2
Shravan Srinivasan <47490781+shravan0409@users.noreply.github.com>
2021-03-18 08:50:28 +0530

    Create triangle quest.py

---
+++ Hackerrank/Math/triangle quest.py
@@ -0,0 +1,14 @@
+for i in range(1,int(input())): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print(int(i*10**i/9))
+    
+    
+# 1
+# 22
+# 333
+# 4444
+# 55555
+# ......
+
+# Logic
+# 1*(10**1/9) = 1
+# 2*(10**2/9) = 22
